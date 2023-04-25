73da51d94ec197c414da0faf9b82a4f1d8044a15
Kristoffer Arrild <ka@peytz.dk>
2023-04-25 21:40:32 +0200

    Cleanup in build stuff.

--- index.d.ts
+++ index.d.ts
@@ -1,17 +1,19 @@
-export interface StyledBlocksStyle {
-    title: string;
-    key: string;
-    background: string;
-    icon: string;
-}
-export declare class StyledBlocksTune {
-    data: string;
-    blockContent: any;
-    styles: StyledBlocksStyle[];
-    constructor({ data, config }: any);
-    static get isTune(): boolean;
-    wrap(blockContent: HTMLElement, style: StyledBlocksStyle): HTMLElement;
-    save(): string;
-    protected addButton(style: StyledBlocksStyle): HTMLElement;
-    render(): HTMLElement;
+declare module 'editorjs-styledblocks' {
+    export interface StyledBlocksStyle {
+        title: string;
+        key: string;
+        background: string;
+        icon: string;
+    }
+    export class StyledBlocksTune {
+        data: string;
+        blockContent: any;
+        styles: StyledBlocksStyle[];
+        constructor({ data, config }: any);
+        static get isTune(): boolean;
+        wrap(blockContent: HTMLElement, style: StyledBlocksStyle): HTMLElement;
+        save(): string;
+        protected addButton(style: StyledBlocksStyle): HTMLElement;
+        render(): HTMLElement;
+    }
 }
